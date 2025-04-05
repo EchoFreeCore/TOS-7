b1a1ee62fd2d2c5332848a7b1b6b7da667bf2270
EchoFreeCore <131709297+EchoFreeCore@users.noreply.github.com>
2025-04-05 14:26:07 +0200

    Update stdlib.h

--- usr/lib/libc/stdlib.h
+++ usr/lib/libc/stdlib.h
@@ -10,7 +10,15 @@ void exit(int status);
 void abort(void);
 int atoi(const char* nptr);
 long strtol(const char* nptr, char** endptr, int base);
-void* malloc(size_t size);
-void free(void* ptr);
+
+void *malloc(size_t size);
+void free(void *ptr);
+void *calloc(size_t nmemb, size_t size);
+void *realloc(void *ptr, size_t size);
+void exit(int status);
+int atexit(void (*function)(void));
+int atoi(const char *nptr);
+long atol(const char *nptr);
+long strtol(const char *nptr, char **endptr, int base);
 
 #endif /* _STDLIB_H */
