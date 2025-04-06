daced2151c9c7f1171b0b105a625ce5ad24c7f54
EchoFreeCore <131709297+EchoFreeCore@users.noreply.github.com>
2025-04-06 21:41:37 +0200

    Update stat.h

--- usr/include/sys/stat.h
+++ usr/include/sys/stat.h
@@ -3,44 +3,79 @@
 
 #include <sys/types.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* --- POSIX struct stat --- */
 struct stat {
-    dev_t     st_dev;
-    ino_t     st_ino;
-    mode_t    st_mode;
-    nlink_t   st_nlink;
-    uid_t     st_uid;
-    gid_t     st_gid;
-    dev_t     st_rdev;
-    off_t     st_size;
-    time_t    st_atime;
-    time_t    st_mtime;
-    time_t    st_ctime;
+    dev_t     st_dev;     /* device ID */
+    ino_t     st_ino;     /* inode number */
+    mode_t    st_mode;    /* file mode */
+    nlink_t   st_nlink;   /* number of hard links */
+    uid_t     st_uid;     /* user ID of owner */
+    gid_t     st_gid;     /* group ID of owner */
+    dev_t     st_rdev;    /* device ID (if special file) */
+    off_t     st_size;    /* total size, in bytes */
+    time_t    st_atime;   /* time of last access */
+    time_t    st_mtime;   /* time of last modification */
+    time_t    st_ctime;   /* time of last status change */
 };
 
-#define S_IFMT  0170000
-#define S_IFREG 0100000
-#define S_IFDIR 0040000
-#define S_IFCHR 0020000
-#define S_IFBLK 0060000
-#define S_IFIFO 0010000
+/* --- File type bits --- */
+#define S_IFMT   0170000 /* bitmask for file type */
+#define S_IFIFO  0010000 /* named pipe (FIFO) */
+#define S_IFCHR  0020000 /* character device */
+#define S_IFDIR  0040000 /* directory */
+#define S_IFBLK  0060000 /* block device */
+#define S_IFREG  0100000 /* regular file */
+#define S_IFLNK  0120000 /* symbolic link (optional POSIX.1-1988) */
+#define S_IFSOCK 0140000 /* socket (optional POSIX.1-1988) */
 
-#define S_ISUID 0004000
-#define S_ISGID 0002000
+/* --- Special mode flags --- */
+#define S_ISUID  0004000 /* set-user-ID bit */
+#define S_ISGID  0002000 /* set-group-ID bit */
 
-#define S_IRWXU 00700
-#define S_IRUSR 00400
-#define S_IWUSR 00200
-#define S_IXUSR 00100
+/* --- User permissions --- */
+#define S_IRWXU  00700    /* read, write, execute/search by owner */
+#define S_IRUSR  00400    /* read permission, owner */
+#define S_IWUSR  00200    /* write permission, owner */
+#define S_IXUSR  00100    /* execute/search permission, owner */
 
-#define S_ISDIR(m)  (((m) & S_IFMT) == S_IFDIR)
+/* --- Group permissions --- */
+#define S_IRWXG  00070
+#define S_IRGRP  00040
+#define S_IWGRP  00020
+#define S_IXGRP  00010
+
+/* --- Others permissions --- */
+#define S_IRWXO  00007
+#define S_IROTH  00004
+#define S_IWOTH  00002
+#define S_IXOTH  00001
+
+/* --- File type testing macros --- */
 #define S_ISREG(m)  (((m) & S_IFMT) == S_IFREG)
+#define S_ISDIR(m)  (((m) & S_IFMT) == S_IFDIR)
+#define S_ISCHR(m)  (((m) & S_IFMT) == S_IFCHR)
+#define S_ISBLK(m)  (((m) & S_IFMT) == S_IFBLK)
+#define S_ISFIFO(m) (((m) & S_IFMT) == S_IFIFO)
+#define S_ISLNK(m)  (((m) & S_IFMT) == S_IFLNK)
+#define S_ISSOCK(m) (((m) & S_IFMT) == S_IFSOCK)
 
-int stat(const char* pathname, struct stat* buf);
-int fstat(int fd, struct stat* buf);
-int chmod(const char* path, mode_t mode);
+/* --- POSIX.1-1988 system calls --- */
+int stat(const char *pathname, struct stat *buf);
+int fstat(int fd, struct stat *buf);
+int lstat(const char *pathname, struct stat *buf); /* optional but common */
+int chmod(const char *path, mode_t mode);
 mode_t umask(mode_t mask);
-int mkdir(const char* pathname, mode_t mode);
+int mkdir(const char *pathname, mode_t mode);
 
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SYS_STAT_H */
 
 
 
