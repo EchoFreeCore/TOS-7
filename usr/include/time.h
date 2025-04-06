d8a396cb6f4dce7ec73f614ffb2b071057be1960
EchoFreeCore <131709297+EchoFreeCore@users.noreply.github.com>
2025-04-06 21:34:54 +0200

    Update time.h

--- usr/include/time.h
+++ usr/include/time.h
@@ -1,26 +1,36 @@
 #ifndef _TIME_H
 #define _TIME_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef long time_t;
 
+/* Broken-down time structure */
 struct tm {
-    int tm_sec;
-    int tm_min;
-    int tm_hour;
-    int tm_mday;
-    int tm_mon;
-    int tm_year;
-    int tm_wday;
-    int tm_yday;
-    int tm_isdst;
+    int tm_sec;   /* seconds [0,60] */
+    int tm_min;   /* minutes [0,59] */
+    int tm_hour;  /* hours [0,23] */
+    int tm_mday;  /* day of month [1,31] */
+    int tm_mon;   /* months since Jan [0,11] */
+    int tm_year;  /* years since 1900 */
+    int tm_wday;  /* day of week [0,6] Sunday = 0 */
+    int tm_yday;  /* day of year [0,365] */
+    int tm_isdst; /* daylight saving time flag */
 };
 
-time_t time(time_t* t);
+/* POSIX.1-1988 time functions */
+time_t time(time_t *t);
 double difftime(time_t end, time_t start);
-char* ctime(const time_t* timep);
+char  *ctime(const time_t *timep);
 struct tm *gmtime(const time_t *timep);
 struct tm *localtime(const time_t *timep);
 unsigned int sleep(unsigned int seconds);
 unsigned int alarm(unsigned int seconds);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _TIME_H */
