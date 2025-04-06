2b2e3643ed77a22061b2e33682683c8c696a0d3b
EchoFreeCore <131709297+EchoFreeCore@users.noreply.github.com>
2025-04-06 21:31:28 +0200

    Update stddef.h

--- usr/include/stddef.h
+++ usr/include/stddef.h
@@ -1,13 +1,32 @@
 #ifndef _STDDEF_H
 #define _STDDEF_H
 
-/* Standard type definitions */
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+/* --- NULL --- */
+#ifndef NULL
 #define NULL ((void *)0)
+#endif
 
-typedef long ptrdiff_t;
-typedef unsigned long size_t;
+/* --- size_t & ptrdiff_t (arkitekturmedveten) --- */
 
-#define offsetof(type, member) ((size_t) &((type *)0)->member)
+#if defined(__x86_64__) || defined(__aarch64__) || defined(_M_X64)
+    typedef unsigned long size_t;
+    typedef long          ptrdiff_t;
+#elif defined(__i386__) || defined(__arm__) || defined(_M_IX86)
+    typedef unsigned int  size_t;
+    typedef int           ptrdiff_t;
+#else
+#   error "Unsupported architecture in <stddef.h>"
+#endif
+
+/* --- offsetof --- */
+#define offsetof(type, member) ((size_t)&(((type *)0)->member))
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _STDDEF_H */
